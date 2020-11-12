2c235aa7736878c73449be726ed5e01bb4b39531
Francisco Power <franciscopower66@gmai.com>
2020-11-12 20:26:52 +0000

    multiple sources working

--- Internet5G/ray_tracer.py
+++ Internet5G/ray_tracer.py
@@ -9,37 +9,32 @@ import math
 def calculateIntensity(src_pos, point, power, bump_map):
 
     # analise das distacias ao scr
-    dist=[]
+    pt_dist = np.zeros((len(src_pos),3))
+
+    #add distances to points
+    for i,t in enumerate(src_pos):
+        pt_dist[i,:] = [t[0], t[1], ((t[0]-point[0])**2 + (t[1]-point[1])**2)]
+        
+    # sort distances and points:
+    pt_dist = pt_dist[np.lexsort((pt_dist[:,1], ))]
 
 
-
-    #???? tem que se corrigir de forma a organizar os vetores d por ordem decresente e que atulize tambem as cordenadas
-    for t in range(0, len(src_pos)):
-        des=src_pos(t)
-        dist.append((des[0]-point[0])**2 + (des[1]-point[1])**2)
-
-        for r in range(0,t):
-            if dist[r]<dist[t]:
-                dist[r]=dist[t]
-                break
-
-
-
-    for d in dist:
+    for d in range(0,pt_dist.shape[0]):
+        x = int(pt_dist[d][0])
+        y = int(pt_dist[d][1])
 
         #check for obstacles
         line = np.zeros(bump_map.shape)
-        line = cv.line(line, src_pos, point, (255), 1)
+        line = cv.line(line, (x,y), point, (255), 1)
         intersection_check = line + bump_map
         line_of_sight = intersection_check[:, :].max() <= 255
 
-
-        if src_pos == point:
+        if (x,y) == (point):
             intensity = power
-        elif bump_map[point[1],point[0]] != 0:
+        elif bump_map[x,y] != 0:
             intensity = 0
         elif line_of_sight:
-            intensity = power / ( 4 * math.pi * d )
+            intensity = power / ( 4 * math.pi * pt_dist[d][2] )
             break
         else:
             intensity = 0
@@ -48,50 +43,39 @@ def calculateIntensity(src_pos, point, power, bump_map):
 
 def main():
 
-    density=1
-
     light_window = "Light"
     bump_map_window = "Bump Map"
-
-    bump_map = cv.imread('bump_map2.png', 0)
-
+    #load windows
+    bump_map = cv.imread('bump_map1.png', 0)
+    #show bumpmap
     cv.imshow(bump_map_window, bump_map)
+    #downscale bumpmap
     original_size = bump_map.shape
     scale_factor = 4
-
-    bump_map = cv.resize(bump_map, (bump_map.shape[1] / scale_factor, bump_map.shape[0] / scale_factor))
-
-    final_visualization = np.zeros(bump_map.shape)
+    bump_map = cv.resize(bump_map, (int(bump_map.shape[1] / scale_factor), int(bump_map.shape[0] / scale_factor)))
+    
+    density=1
     intensity_values = np.zeros(bump_map.shape)
 
+    #router properties
     power = 100
-    # src_pos = (bump_map.shape[1]/2,bump_map.shape[0]/2)
-    # density = 2
-
     # list of position of router
-
-    src_pos=[(30,10), (20, 20), (39,59)]
-
-
+    src_pos=[(30,10), (50, 50), (100,100)]
+    # src_pos=[(30,10)]
 
 
-    intensity_values = np.zeros(bump_map.shape)
-
+    # calculate intensity matrix
     for l in range(0, bump_map.shape[0] - 1, density):
         for c in range(0, bump_map.shape[1] - 1, density):
             intensity = calculateIntensity(src_pos,(c,l), power, bump_map)
             intensity_values[l][c] = intensity
 
+    #visualize result
     final_visualization = intensity_values * 255 / power * 10
-
     final_visualization = cv.resize(final_visualization, (original_size[1], original_size[0]))
-
     cv.imshow(light_window, final_visualization)
 
-    k = cv.waitKey(0)
-
-
-cv.waitKey(0)
+    cv.waitKey(0)
 
         
         
