9ede207b17180b8b864c3087f7ad7cff7f057c39
Francisco Power <franciscopower66@gmai.com>
2020-11-12 23:01:46 +0000

    second interactive source

--- Internet5G/ray_tracer.py
+++ Internet5G/ray_tracer.py
@@ -6,43 +6,72 @@ from functools import partial
 import cv2 as cv
 import numpy as np
 
-src_pos = (5, 5)
-moving = False
+src_pos = [(5, 5), (80,80)]
+moving1 = False
+moving2 = False
 density = 1
 
 def onClick(event, x, y, flags, param):
-    global moving, src_pos, density
+    global moving1, moving2, src_pos, density
     if event == cv.EVENT_LBUTTONDOWN:
-        src_pos = (x,y)
-        moving = True
+        src_pos[0] = (x,y)
+        moving1 = True
     elif event == cv.EVENT_LBUTTONUP:
-        src_pos = (x,y)
+        src_pos[0] = (x,y)
         density = 1
-        moving = False
+        moving1 = False
+        
+    if event == cv.EVENT_RBUTTONDOWN:
+        src_pos[1] = (x,y)
+        moving2 = True
+    elif event == cv.EVENT_RBUTTONUP:
+        src_pos[1] = (x,y)
+        density = 1
+        moving2 = False
     
-    if event == cv.EVENT_MOUSEMOVE and moving:
-        src_pos = (x,y)
-        density = 2
+    if event == cv.EVENT_MOUSEMOVE:
+        if moving1:
+            src_pos[0] = (x,y)
+            density = 2
+        elif moving2:
+            src_pos[1] = (x,y)
+            density = 2
+        
         
                             
 def calculateIntensity(src_pos, point, power, bump_map):
-    
-    #check for obstacles
-    line = np.zeros(bump_map.shape)
-    line = cv.line(line, src_pos, point, (255), 1)
-    intersection_check = line + bump_map
-    line_of_sight = intersection_check[:, :].max() <= 255
-    
-    
-    if src_pos == point:
-        intensity = power
-    elif bump_map[point[1],point[0]] != 0:
-        intensity = 0
-    elif line_of_sight:
-        intensity = power / ( 4 * math.pi * ((src_pos[0]-point[0])**2 + (src_pos[1]-point[1])**2) )
-    else:
-        intensity = 0
+
+    # analise das distacias ao scr
+    pt_dist = np.zeros((len(src_pos),3))
+
+    #add distances to points
+    for i,t in enumerate(src_pos):
+        pt_dist[i,:] = [t[0], t[1], ((t[0]-point[0])**2 + (t[1]-point[1])**2)]
         
+    # sort distances and points:
+    pt_dist = pt_dist[np.lexsort((pt_dist[:,2], ))]
+
+
+    for d in range(0,pt_dist.shape[0]):
+        x = int(pt_dist[d][0])
+        y = int(pt_dist[d][1])
+
+        #check for obstacles
+        line = np.zeros(bump_map.shape)
+        line = cv.line(line, (x,y), point, (255), 1)
+        intersection_check = line + bump_map
+        line_of_sight = intersection_check[:, :].max() <= 255
+
+        if (x,y) == (point):
+            intensity = power
+        elif bump_map[point[1],point[0]] != 0:
+            intensity = 0
+        elif line_of_sight:
+            intensity = power / ( 4 * math.pi * pt_dist[d][2] )
+            break
+        else:
+            intensity = 0
+
     return intensity
 
 def main():
@@ -55,7 +84,7 @@ def main():
     original_size = bump_map.shape
     scale_factor = 4
     
-    bump_map = cv.resize(bump_map, (bump_map.shape[1]/scale_factor, bump_map.shape[0]/scale_factor))
+    bump_map = cv.resize(bump_map, (int(bump_map.shape[1]/scale_factor), int(bump_map.shape[0]/scale_factor)))
     
     final_visualization = np.zeros(bump_map.shape)
     intensity_values = np.zeros(bump_map.shape)
@@ -70,10 +99,13 @@ def main():
         cv.setMouseCallback(light_window, onClick)
         
         intensity_values = np.zeros(bump_map.shape)
+        
+        src_pos_scaled = [(int(src_pos[0][0]/scale_factor),int(src_pos[0][1]/scale_factor)),(int(src_pos[1][0]/scale_factor),int(src_pos[1][1]/scale_factor))]
+        
 
         for l in range(0,bump_map.shape[0]-1,density):
             for c in range(0,bump_map.shape[1]-1,density):
-                intensity = calculateIntensity((src_pos[0]/scale_factor, src_pos[1]/scale_factor), (c,l), power, bump_map)
+                intensity = calculateIntensity(src_pos_scaled, (c,l), power, bump_map)
                 intensity_values[l][c] = intensity
                 
         final_visualization = intensity_values*255/power *10
