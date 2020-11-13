9671e459692f16677075fdbcac34d37f18f30a63
Pedro Rolo <pmrolo@ua.pt>
2020-11-13 00:17:21 +0000

    elaboraçao do programa funcao objtivo

--- Internet5G/ray_tracer.py
+++ Internet5G/ray_tracer.py
@@ -41,26 +41,29 @@ def calculateIntensity(src_pos, point, power, bump_map):
 
     return intensity
 
-def intensityMatrix():
+def intensityMatrix(bump_map,src_pos,power,scale_factor):
 
-    light_window = "Light"
-    bump_map_window = "Bump Map"
+    #light_window = "Light"
+    #bump_map_window = "Bump Map"
     #load windows
-    bump_map = cv.imread('bump_map1.png', 0)
+    #bump_map = cv.imread('bump_map1.png', 0)
     #show bumpmap
-    cv.imshow(bump_map_window, bump_map)
+    #cv.imshow(bump_map_window, bump_map)
     #downscale bumpmap
     original_size = bump_map.shape
-    scale_factor = 4
+    #scale_factor = 4
+
     bump_map = cv.resize(bump_map, (int(bump_map.shape[1] / scale_factor), int(bump_map.shape[0] / scale_factor)))
     
     density=1
     intensity_values = np.zeros(bump_map.shape)
 
     #router properties
-    power = 100
+
+    #power = 100
     # list of position of router
-    src_pos=[(30,10), (50, 50), (100,100)]
+
+    #src_pos=[(30,10), (50, 50), (100,100)]
     # src_pos=[(30,10)]
 
 
@@ -71,12 +74,15 @@ def intensityMatrix():
             intensity_values[l][c] = intensity
 
     #visualize result
-    final_visualization = intensity_values * 255 / power * 10
-    final_visualization = cv.resize(final_visualization, (original_size[1], original_size[0]))
-    cv.imshow(light_window, final_visualization)
+    #final_visualization = intensity_values * 255 / power * 10
+    #final_visualization = cv.resize(final_visualization, (original_size[1], original_size[0]))
 
-    cv.waitKey(0)
+    #cv.imshow(light_window, final_visualization)
 
+    #cv.waitKey(0)
+
+
+    return (intensity_values)
         
         
 
