93a7477c1aeac1aaccbf01363240f07e7dfbaaea
Francisco Power <franciscopower66@gmai.com>
2020-11-19 12:46:54 +0000

    created interpretGCode.py

---
+++ Impressao3D/interpretGCode.py
@@ -0,0 +1,15 @@
+
+
+def interpretGCode():
+
+    path = 'Impressao3D/barco'
+    f = open(path, 'r')
+    data = f.read()
+
+    print(data)
+    
+    
+    
+    
+if __name__ == "__main__":
+    interpretGCode()
