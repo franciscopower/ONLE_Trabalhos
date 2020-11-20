b770b7b6e57ba5df7084cc03d437255117f99944
Francisco Power <franciscopower66@gmai.com>
2020-11-20 00:48:56 +0000

    programa para interpretar gcode

--- Impressao3D/interpretGCode.py
+++ Impressao3D/interpretGCode.py
@@ -1,15 +1,81 @@
+#!/usr/bin/python 
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
 
 
 def interpretGCode():
 
-    path = 'Impressao3D/barco'
+    path = 'Impressao3D/barcominimalista.gcode'
     f = open(path, 'r')
     data = f.read()
 
-    print(data)
+    line = data.split('\n')
+    
+    printing = False
+    
+    all_pts = np.array([
+                [],
+                [],
+                [],
+            ])
+    
+    in_pts = np.array([
+                [],
+                [],
+                [],
+            ])
+    
+    out_pts = np.array([
+                [],
+                [],
+                [],
+            ])
+    
+    pt = np.array([
+                [0.0],
+                [0.0],
+                [0.0],
+            ])
     
     
+    for i,l in enumerate(line):
+        if 'Z0' in l or 'TYPE' in l:
+            printing = True
+            
+        if 'FILL' in l:
+            printing = False
+            
+        if not 'G0' in l and not 'G1' in l:
+            continue
+            
+        if printing:
+            word = l.split(' ')
+            
+            if 'Z' in l and i != 0:
+                out_pts = np.append(out_pts, pt, 1)
+            
+            for w in word:
+                if 'X' in w:
+                    pt[0,0] = float(w[1:])       
+                elif 'Y' in w:
+                    pt[1,0] = float(w[1:])        
+                elif 'Z' in w:
+                    pt[2,0] = float(w[1:])
+                    
+            if 'Z' in l:
+                in_pts = np.append(in_pts, pt, 1)
+
+            all_pts = np.append(all_pts, pt, 1)
     
-    
+    out_pts = np.append(out_pts, pt, 1)
+          
+    plt.figure()
+    ax = plt.axes(projection='3d')
+
+    ax.plot3D(all_pts[0,:], all_pts[1,:], all_pts[2,:])
+    plt.show()
+       
+        
 if __name__ == "__main__":
     interpretGCode()
