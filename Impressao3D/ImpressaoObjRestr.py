08894d23c76734c20484e6c5f44586fd7f868f76
Francisco Power <franciscopower66@gmai.com>
2020-11-21 18:45:15 +0000

    correcao algoritomo de medicao de distancias

--- Impressao3D/ImpressaoObjRestr.py
+++ Impressao3D/ImpressaoObjRestr.py
@@ -4,41 +4,61 @@ import interpretGCode
 def objFunction(objs):
     n_layers = [o[1].shape[1] for o in objs] #numero de camadas de cada objeto
     
+    total_obj_idx = range(0, len(objs))
     obj_n = 0 #primeiro objeto a testar
-    tested_objs = [obj_n] #indice dos objetos que ja foram testados
     d_min = 200 #distancia para comparacao
     d_total = 0
     #percorrer cada camada
     for l in range(0, max(n_layers)):
+        obj_idx = total_obj_idx[:]
+    
+        n_valid_objs = len(total_obj_idx)
+        
         #comecar no primeiro objeto, ir de objeto em objeto
-        for _ in range(0,len(objs)): 
+        while True: 
+            if n_valid_objs == 1:
+                break
+            n_valid_objs -= 1
+            obj_idx.pop(obj_idx.index(obj_n))
+            if not obj_idx:
+                continue
+            
+            #get out position of current object
             try:
                 out = objs[obj_n][2][:,l]
             except:
                 out = objs[obj_n][2][:,l-1]   
                 
-            for n,o in enumerate(objs):
-                
-                if n in tested_objs:
+            for i in obj_idx:
+                #check if object has current layer
+                if objs[i][1].shape[1] == l:
+                    try:
+                        total_obj_idx.pop(total_obj_idx.index(i))
+                    except:
+                        pass
+                    else:
+                        n_valid_objs -= 1
                     continue
                 
-                try:
-                    inn = o[1][:,l]
-                except:
-                    continue
-                else:
-                    d = np.sqrt((out[0]-inn[0])**2 + (out[1]-inn[1])**2)
-                    
-                    if d <= d_min:
-                        d_min = d
-                        obj_n = n
+                #get in position of next object
+                inn = objs[i][1][:,l]
+                #calculate distance
+                d = np.sqrt((out[0]-inn[0])**2 + (out[1]-inn[1])**2)
+                
+                #check if distance is smaller that with the previous object
+                if d <= d_min:
+                    d_min = d
+                    obj_n_temp = i
             
+            #update current object and total distance
+            obj_n = obj_n_temp
             d_total += d_min
+            print(l, d_min)
+            #reset minimum distance
+            d_min = 200
 
-            tested_objs.append(obj_n)
-                
-        obj_n = tested_objs[-1]    
-        tested_objs = [obj_n]  
+            
+            
           
     return d_total
 
