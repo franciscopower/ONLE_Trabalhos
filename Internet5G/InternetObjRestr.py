76ca3f252e33b004643d047178f8e5cf12454d4e
Francisco Power <franciscopower66@gmai.com>
2020-11-25 21:41:34 +0000

    show result in 5G

--- Internet5G/InternetObjRestr.py
+++ Internet5G/InternetObjRestr.py
@@ -49,6 +49,7 @@ def main():
     #-----------------------------
     
     #reduce size of bump map for faster analisis
+    original_size = bump_map.shape
     scale_factor = 4
     bump_map = cv.resize(bump_map, (int(bump_map.shape[1] / scale_factor), int(bump_map.shape[0] / scale_factor)))
     
@@ -59,7 +60,7 @@ def main():
     restriction_min_intensity = restriction_intesity_min(intensity_matrix,bump_map, power, value_min)
     
     _, bump_map_inv = cv.threshold(bump_map, 5, 255, cv.THRESH_BINARY_INV)
-    restriction_position = restriction_verify_position(bump_map_inv, src_pos)
+    restriction_position = restriction_verify_position(bump_map_inv, src_pos) #! aqui sera usada uma imagem propria, allowed_position, nao o inverso do bump_map
     
     #Calculate objective function
     funcao_objtivo = objFunction(intensity_matrix,bump_map_inv)
@@ -69,10 +70,14 @@ def main():
     print('Minimum intensity restriction: ' + str(restriction_min_intensity))
     print('Position restriction: ' + str(restriction_position))
     
-    #Show image
-    cv.imshow('tata',bump_map)
-    cv.imshow('tata2',bump_map_inv)
-
+    #visualize result
+    final_visualization = intensity_matrix * 255 / power * 10
+    final_visualization = cv.resize(final_visualization, (original_size[1], original_size[0]))
+    cv.imshow('light_window', final_visualization)
+    
+    bump_map = cv.resize(bump_map,  (original_size[1], original_size[0]))
+    cv.imshow('bump_map_window', bump_map)
+    
     cv.waitKey(0)
 
 if __name__ == "__main__":
