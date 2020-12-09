8b1c7dad58cb41c690107c23d7604fad1bb083fe
Pedro Rolo <pmrolo@ua.pt>
2020-12-09 21:21:10 +0000

    terminar levy

--- ES.py
+++ ES.py
@@ -1,4 +1,4 @@
-import numpy as np
+import numpy as np, random
 from math import gamma, sin, pi
 
 def sphere(x):
@@ -8,6 +8,9 @@ def sphere(x):
 def levy(l, varsize):
     xvar = (gamma(1+l) * sin(pi * l / 2) / gamma((1 + l)/2 * l * 2**((l-1)/2)) )**(2/l)
     yvar = 1
+    x=random.normal(loc=0.0, scale=xvar, size=varsize)
+    y=random.normal(loc=0.0, scale=yvar, size=varsize)
+    v=x/(abs(y)**(1/l))
 
 
 def eagleStrategy(problem, param):
